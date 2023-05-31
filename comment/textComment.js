aeb2e48b39e1609f8d57beae7cce5552b57b4ccd
{ThongKieu} <{kieuthong16dth@gmail.com}>
2023-05-31 15:37:55 +0700

    upate

---
+++ comment/textComment.js
@@ -0,0 +1,14 @@
+const replyCmt = document.getElementById('replyCmtTV');
+    replyCmt.addEventListener('click',function (){
+      document.getElementById('replyCmt').style.display = 'block';
+    })
+function calcHeight(value) {
+    let numberOfLineBreaks = (value.match(/\n/g) || []).length;
+    // min-height + lines x line-height + padding + border
+    let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+    return newHeight;
+  }
+let textarea = document.querySelector(".resize-ta");
+textarea.addEventListener("keyup", () => {
+  textarea.style.height = calcHeight(textarea.value) + "px";
+});
---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBl5rDC-JcMqyi0Ds7xQWkDRXwW7rFAa0A",
+    authDomain: "appthoviet.firebaseapp.com",
+    databaseURL: "https://appthoviet-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "appthoviet",
+    storageBucket: "appthoviet.appspot.com",
+    messagingSenderId: "886027498015",
+    appId: "1:886027498015:web:e641dc105b199eca8a5b2a",
+    measurementId: "G-LJJ8QGQ92R"
+  };
